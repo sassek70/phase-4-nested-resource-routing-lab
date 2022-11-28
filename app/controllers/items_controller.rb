8f40206a568931a5789349636b8d48d45b85cfa2
Kevin Sasse <sassek70@gmail.com>
2022-11-28 17:15:35 -0500

    commit tree 0394884fea9c323ff81aab73dc39295da77a147b

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,50 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    # user = find_user
+    item = find_item  
+    render json: item, include: :user
+  end
+
+  def create
+    user = find_user
+    new_item = Item.create(item_params)
+    render json: new_item, status: :created
+  end
+
+
+
+
+
+
+
+  private
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
   end
 
 end
